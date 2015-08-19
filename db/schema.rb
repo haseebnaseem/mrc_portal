530ed1b1084f65ebac3af7eecc66f499dc3d1a7a
Haseeb Naseem <18100226@lums.edu.pk>
2015-08-19 10:20:43 +0000

    student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818120635) do
+ActiveRecord::Schema.define(version: 20150819101507) do
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.string   "refrence"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
