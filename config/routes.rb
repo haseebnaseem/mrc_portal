a6bcf4567374d970123f43c821099658f529a20a
Haseeb Naseem <18100226@lums.edu.pk>
2015-08-19 08:31:17 +0000

    full login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'signup' => 'users#new'
+  get '/signup' => 'users#new'
   resources :users 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#des'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
