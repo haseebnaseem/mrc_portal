8b7da833793bc74f82c6bed92e7de46dd6dc4630
Haseeb Naseem <18100226@lums.edu.pk>
2015-08-19 09:17:39 +0000

    trying to get heroku to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def new
     @user = User.new
-  end
+    end
    def create 
   @user = User.new(user_params) 
   if @user.save 
@@ -15,4 +15,5 @@ end
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
+
 end
